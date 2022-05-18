0e52da742232c111c01a85192e6d92c80d4ad4e4
Shrinivasraddi <97089356+Shrinivasraddi@users.noreply.github.com>
2022-05-18 18:58:56 +0000

    9

---
+++ problem#9.1.py
@@ -0,0 +1,20 @@
+#lists
+fname = input("Enter file name: ")
+if len(fname) < 1:
+    fname = "mbox-short.txt"
+
+maharshi = open(fname)
+h=list()
+count = 0
+for line in maharshi:
+    if not line.startswith("From "):
+        
+        continue
+    if line.startswith("From"):
+        #print(m)
+        maggy=line.split()
+        print(maggy[1])
+        
+	count=count+1
+
+print('There were',count,'lines in the file with From as the first word')
