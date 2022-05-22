b3c349c66d98c6c2869500e5fad2131ab554a72c
Shrinivasraddi <97089356+Shrinivasraddi@users.noreply.github.com>
2022-05-22 18:00:18 +0000

    11

--- ActivitySet#1/problem#11.py
+++ ActivitySet#1/problem#11.py
@@ -1,3 +1,20 @@
 # Tuples
 
-filename = "dataset/mbox-short.txt"
+#filename = "dataset/mbox-short.txt"
+file = open('mbox-short.txt')
+di={}
+for line in file:
+    if line.startswith('From '):
+        line.rstrip
+        words=line.split()
+        w=words[5]
+        
+        k=w[:2]
+        di[k]=di.get(k,0)+1 
+
+t=[]
+for key,value in di.items():
+    t.append((key,value))
+t=sorted(t)
+for key,value in t:
+    print (key,value)
