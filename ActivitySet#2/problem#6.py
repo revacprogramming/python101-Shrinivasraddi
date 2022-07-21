6b6bdd3744ddae3225e21bd860f83f0b01bd5e3e
Shrinivasraddi <97089356+Shrinivasraddi@users.noreply.github.com>
2022-07-21 19:00:34 +0000

    ,

--- ActivitySet#2/problem#6.py
+++ ActivitySet#2/problem#6.py
@@ -1,11 +1,10 @@
-
-
 class Menu:
-    """fill in class definition here"""
+  """fill in class definition here"""
+  def add(self,food,qty):
+    print(food,qty)
 
+m = Menu()   # Menu is a class
 
-m = Menu()  # Menu is a class
-m.add("idly", 10)
-m.add("vada". 20)
+m.add("idly", 10)  
 
-m.show()
+m.add("vada", 20)
